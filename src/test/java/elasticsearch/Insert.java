5416a2316f5b97fc72144f7d62056aaa9dc6d186
Christoph Suter <christoph.suter@noser.com>
2015-05-06 14:00:22 +0200

    some stuff

--- src/test/java/elasticsearch/Insert.java
+++ src/test/java/elasticsearch/Insert.java
@@ -5,10 +5,17 @@ import static org.elasticsearch.common.xcontent.XContentFactory.jsonBuilder;
 import java.io.IOException;
 import java.util.Date;
 
-import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.action.get.GetResponse;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.action.search.SearchType;
 import org.elasticsearch.client.Client;
 import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.common.transport.InetSocketTransportAddress;
+import org.elasticsearch.index.query.FilterBuilders;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Ignore;
 /*
  import org.elasticsearch.ElasticsearchException;
  import org.elasticsearch.action.index.IndexResponse;
@@ -21,30 +28,59 @@ import org.junit.Test;
 
 public class Insert {
 
-	String json = "{" + "\"user\":\"kimchy\"," + //
-			"\"postDate\":\"2013-01-30\"," + //
-			"\"message\":\"trying out Elasticsearch\"" + //
-			"}";
+	Client client;
 
-	@Test
-	public void test() throws IOException {
-		// Node node = nodeBuilder().clusterName("localhost").node();
-		// Client client = node.client();
+	@Before
+	public void setup() {
+		client = getClient();
+	}
 
+	@After
+	public void teardown() {
+		client.close();
+	}
+
+	private Client getClient() {
 		Client client = new TransportClient().addTransportAddress(
 				new InetSocketTransportAddress("localhost", 9300))
 				.addTransportAddress(
 						new InetSocketTransportAddress("127.0.0.1", 9300));
-		System.out.println(client);
+		System.out.println("Client connected.");
 
-		IndexResponse response = client
-				.prepareIndex("twitter", "tweet", "1")
+		return client;
+	}
+
+	@Test
+	public void tetsSearch() {
+		SearchResponse response = client.prepareSearch("twitter")
+				.setTypes("tweet")
+				.setSearchType(SearchType.DFS_QUERY_THEN_FETCH)
+				.setQuery(QueryBuilders.termQuery("multi", "test"))
+				// Query
+				.setPostFilter(FilterBuilders.rangeFilter("ID").from(1).to(32)) // Filter
+				.setFrom(0).setSize(60).setExplain(true).execute().actionGet();
+	}
+
+	@Test
+	public void tetsGet() {
+		GetResponse response = client.prepareGet("twitter", "tweet", "31")
+				.execute().actionGet();
+
+		System.out.println(response.getHeaders());
+		System.out.println(response.getFields());
+	}
+
+	@Ignore
+	public void testIndex() throws IOException {
+		int randomIndex = (int) (Math.random() * 100);
+
+		client.prepareIndex("twitter", "tweet", "" + randomIndex)
 				.setSource(
 						jsonBuilder().startObject().field("user", "kimchy")
 								.field("postDate", new Date())
 								.field("message", "trying out Elasticsearch")
 								.endObject()).execute().actionGet();
 
-		client.close();
+		System.out.println("Index added: " + randomIndex);
 	}
 }
